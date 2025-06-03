a26a85f4d64902ec6607675c8c1715078a52151f
Vavilion03 <aiym.hen@gmail.com>
2025-06-03 22:31:36 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет, я тут")
-
-app = ApplicationBuilder().token("7698307866:AAGyAsVLM4h6jysuvDOMNnlq2nezHxIx7f0").build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
